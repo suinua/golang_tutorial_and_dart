0d47f8ad79b3621db86a0b819ec6f53adf04d7ad
suinua <maatinosippo@gmail.com>
2019-03-27 18:17:54 +0900

    add multiple_results folder

---
+++ src/multiple_results/main.dart
@@ -0,0 +1,11 @@
+import 'package:tuple/tuple.dart';
+
+//DartにTupleは無い。
+void main(List<String> args) {
+  Tuple2 x = swap(Tuple2("Hello", "World"));
+  print(x);
+}
+
+Tuple2 swap(Tuple2 tuple) {
+  return Tuple2(tuple.item2,tuple.item1);
+}
