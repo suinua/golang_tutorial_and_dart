885f29da7b4591c9b89b050f96c7b63834296fc2
suinua <maatinosippo@gmail.com>
2019-03-27 16:38:05 +0900

    add import_pacakge folder

---
+++ src/import_package/main.dart
@@ -0,0 +1,5 @@
+import 'dart:math';
+
+void main(List<String> args) {
+  print("My favorite number is ${Random().nextInt(10)}");
+}
---
+++ src/import_package/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+/*
+import "fmt"
+import "math/rand"
+でも可。
+*/
+
+func main() {
+	fmt.Println("My favorite number is", rand.Intn(10))
+}
