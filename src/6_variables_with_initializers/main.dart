63b1c095bbecadf754caf3dbde9e91ebdbc2c980
suinua <maatinosippo@gmail.com>
2019-03-27 18:33:35 +0900

    add variables_with_initializers folder

---
+++ src/6_variables_with_initializers/main.dart
@@ -0,0 +1,11 @@
+int i = 1, j = 2;
+
+void main(List<String> args) {
+  var c = true, python = false, java = "no!";
+
+  print("i = $i");
+  print("j = $j");
+  print("c = $c");
+  print("python = $python");
+  print("java = $java");
+}
---
+++ src/6_variables_with_initializers/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+var i, j int = 1, 2
+
+func main() {
+	var c, python, java = true,false,"no!"
+
+	fmt.Println("i =",i)
+	fmt.Println("j =",j)
+	fmt.Println("c =", c)
+	fmt.Println("python =", python)
+	fmt.Println("java =", java)
+}
