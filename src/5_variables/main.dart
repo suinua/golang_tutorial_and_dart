4a0bb13ddfb38e29c13fa5bb186b206c58330d9f
suinua <maatinosippo@gmail.com>
2019-03-27 18:28:03 +0900

    add variables folder

---
+++ src/5_variables/main.dart
@@ -0,0 +1,11 @@
+
+bool c,python,java;
+
+void main(List<String> args) {
+  bool i;
+  print(i);
+
+  print("c = $c");
+  print("python = $python");
+  print("java = $java");
+}
---
+++ src/5_variables/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var c, python, java bool
+
+func main() {
+	var i bool
+	fmt.Println(i)
+	fmt.Println("c =", c)
+	fmt.Println("python =", python)
+	fmt.Println("java =", java)
+}
