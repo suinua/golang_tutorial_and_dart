f145c05c9806b11835a999766179034eee24fa6f
suinua <maatinosippo@gmail.com>
2019-03-28 17:29:28 +0900

    add constants folder

---
+++ src/10_constants/main.dart
@@ -0,0 +1,11 @@
+
+const pi = 3.14;
+
+void main(List<String> args) {
+  const world = "世界";
+  print("Hello " + world);
+  print("Happy $pi Day");
+
+  const truth = true;
+  print("Go rules? $truth");
+}
