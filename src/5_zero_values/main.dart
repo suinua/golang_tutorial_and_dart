4f4657fffd411c9ab11cd5f1b9de2bbbbb8a8abe
suinua <maatinosippo@gmail.com>
2019-03-28 14:35:23 +0900

    リネームとコード変更

---
+++ src/5_zero_values/main.dart
@@ -0,0 +1,11 @@
+String s;
+int i;
+double d;
+bool b;
+
+void main(List<String> args) {
+  print("s = $s");
+  print("i = $i");
+  print("d = $d");
+  print("b = $b");
+}
---
+++ src/5_zero_values/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+var (
+	s string
+	i int
+	d float64
+	b bool
+)
+
+func main() {
+	fmt.Println("s =", s)
+	fmt.Println("i =", i)
+	fmt.Println("d =", d)
+	fmt.Println("b =", b)
+}
