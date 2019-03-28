a3f3b5c71ae4580a90ef1c3280988290a725f7e6
suinua <maatinosippo@gmail.com>
2019-03-28 14:56:40 +0900

    add basic_types folder

---
+++ src/7_basic_types/main.dart
@@ -0,0 +1,11 @@
+String s = "";
+int i = 10;
+double d = 10.0;
+bool b = false;
+
+void main(List<String> args) {
+  print("Type: ${s.runtimeType}, Value: $s");
+  print("Type: ${i.runtimeType}, Value: $i");
+  print("Type: ${d.runtimeType}, Value: $d");
+  print("Type: ${b.runtimeType}, Value: $b");
+}
